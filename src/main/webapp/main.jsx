2037059db45cf2eb78b96f9b0fcbe5ca14d93c9e
Thaíssa Candella <thaissa.ivrit@gmail.com>
2016-10-10 15:30:50 -0300

    Integrating yawp

--- src/main/webapp/main.jsx
+++ src/main/webapp/main.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import {render} from 'react-dom';
+import yawp from 'yawp';
 
 class App extends React.Component {
+ constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentWillMount() {
+    console.log('hue');
+    yawp('/especies').where('currency', '=', 'BRL').list(especies => {
+        this.setState({ especies });
+    });
+  }
+
   render () {
-    return <p> Hello React!</p>;
+    if (this.state.especies) {
+        return <p>{JSON.stringify(this.state.especies)}</p>;
+    } else {
+        return <p>Loading!</p>;
+    }
   }
 }
 
