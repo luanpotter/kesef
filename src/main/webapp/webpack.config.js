464f439bd62653569039e5e6b0536e975e4f45e8
Thaíssa Candella <thaissa.ivrit@gmail.com>
2016-10-10 14:41:50 -0300

    Adding react, babel, etdc

--- src/main/webapp/webpack.config.js
+++ src/main/webapp/webpack.config.js
@@ -1,7 +1,18 @@
 module.exports = {
-    entry: './main.js',
+    entry: './main.jsx',
     output: {
         path: './',
         filename: 'bundle.js'
+    },
+    module : {
+      loaders : [
+        {
+          test : /\.jsx?$/,
+          loader: 'babel-loader',
+          query: {
+               presets: ['es2015']
+          }
+        }
+      ]
     }
 };
