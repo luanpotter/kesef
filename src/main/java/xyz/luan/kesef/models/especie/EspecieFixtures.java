84a91d168b0f1c049666b84198cb7cf0bb5afe9e
Luan Nico <luanpotter27@gmail.com>
2016-10-17 23:35:08 -0200

    Adding all fixtures

--- src/main/java/xyz/luan/kesef/models/especie/EspecieFixtures.java
+++ src/main/java/xyz/luan/kesef/models/especie/EspecieFixtures.java
@@ -7,7 +7,18 @@ public class EspecieFixtures {
 
     public static List<Especie> all() {
         List<Especie> es = new ArrayList<>();
+        es.add(brl(5, Type.MOEDA));
+        es.add(brl(10, Type.MOEDA));
+        es.add(brl(25, Type.MOEDA));
+        es.add(brl(50, Type.MOEDA));
         es.add(brl(100, Type.MOEDA));
+
+	es.add(brl(200, Type.PAPEL));
+	es.add(brl(500, Type.PAPEL));
+	es.add(brl(1000, Type.PAPEL));
+	es.add(brl(2000, Type.PAPEL));
+	es.add(brl(5000, Type.PAPEL));
+	es.add(brl(10000, Type.PAPEL));
         return es;
     }
 
