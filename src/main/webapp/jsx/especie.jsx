e2adf7feda9154c667d543540fc1fb9bfe3e80ac
Luan Nico <luanpotter27@gmail.com>
2016-10-18 00:38:45 -0200

    CSS and touches

--- src/main/webapp/jsx/especie.jsx
+++ src/main/webapp/jsx/especie.jsx
@@ -1,15 +1,43 @@
 import React from 'react';
+import numeral from 'numeral';
 
 const IMGS = {};
 IMGS.MOEDA = require('../images/coins.png');
 IMGS.PAPEL = require('../images/notes.png');
 
+var inlineBlock = {
+  display: 'inline-block'
+};
+
+var especiesStyle = {
+  borderRadius: '16px',
+  border: '1px solid gray',
+  backgroundColor: 'white',
+  margin: '4px',
+  padding: '1px',
+  width: 'calc(33% - 2*4px)',
+  color: 'black',
+  display: 'inline-block',
+  fontSize: '2em',
+  cursor: 'pointer'
+};
+
+var smallText = {
+  fontSize: '0.3em'
+};
+
 class Especie extends React.Component {
   render () {
-    return <div className="especie">
-      <span className="currency">{ this.props.currency }</span>
-      <span className="type"><img src={IMGS[this.props.type]} /></span>
-      <span className="value">{ this.props.centsValue }</span>
+    return <div className="especie" style={ especiesStyle }>
+      <div className="type" style={ inlineBlock }><img src={IMGS[this.props.type]} /></div>
+      <div style={ inlineBlock }>
+        <div>
+          <span className="currency">{ this.props.currency } </span>
+          <span className="value">{ numeral(this.props.centsValue/100).format('0.00') }</span>
+        </div><div style={ smallText }>
+          <span className="amount">Own: 100 | Buy: 20</span>
+        </div>
+      </div>
     </div>;
   }
 }
